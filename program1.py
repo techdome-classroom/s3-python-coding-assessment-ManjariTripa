eade9477daba8221eb107f987ad426a59f7908d4
Manjari Tripathi <114255847+ManjariTripa@users.noreply.github.com>
2024-10-07 15:46:09 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,10 +1,13 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stk = []
+        d = {'()', '[]', '{}'}
+        for c in s:
+            if c in '({[':
+                stk.append(c)
+            elif not stk or stk.pop() + c not in d:
+                return False
+        return not stk
 
 
 
